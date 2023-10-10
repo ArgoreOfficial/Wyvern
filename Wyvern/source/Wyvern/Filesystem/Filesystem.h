ac285b4085ce7d24d7b62cc76d32384a26f647af
Argore <argoreyt@gmail.com>
2023-10-10 23:11:44 +0200

    changed to std::string

--- Wyvern/source/Wyvern/Filesystem/Filesystem.h
+++ Wyvern/source/Wyvern/Filesystem/Filesystem.h
@@ -12,10 +12,19 @@ namespace WV
 			std::ifstream infile( _path );
 			if ( _printError && !infile.good() )
 			{
-				WVERROR( "File not found:\n    %s", _path );
+				WVERROR( "File not found:\n    %s", _path.c_str() );
 			}
 
 			return infile.good();
+		}
+
+		static std::string getFilenameFromPath( std::string _path )
+		{
+			std::string filename = _path.substr( _path.find_last_of( "/\\" ) + 1 );
+			// std::string::size_type const p( filename.find_last_of( '.' ) );
+			// std::string withoutExtension = filename.substr( 0, p );
+			
+			return filename.c_str();
 		}
 	};
 }
