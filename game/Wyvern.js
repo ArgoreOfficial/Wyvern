789ce36eea7d3b70250aae21a58b1867bde489ba
Pi <argoreyt@gmail.com>
2024-05-28 14:12:38 +0200

    merged glad and glad-es
    the glad generator can do both in the same file anyways

--- game/Wyvern.js
+++ game/Wyvern.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\piostb\AppData\Local\Temp\tmpw3wmqv58.js
+// include: C:\Users\piostb\AppData\Local\Temp\tmpf9m1lg6c.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -220,21 +220,21 @@ Module['FS_createPath']("/", "res", true, true);
 
   })();
 
-// end include: C:\Users\piostb\AppData\Local\Temp\tmpw3wmqv58.js
-// include: C:\Users\piostb\AppData\Local\Temp\tmpnb7ks4hq.js
+// end include: C:\Users\piostb\AppData\Local\Temp\tmpf9m1lg6c.js
+// include: C:\Users\piostb\AppData\Local\Temp\tmpq26hp1iq.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\piostb\AppData\Local\Temp\tmpnb7ks4hq.js
-// include: C:\Users\piostb\AppData\Local\Temp\tmpzotb0y61.js
+  // end include: C:\Users\piostb\AppData\Local\Temp\tmpq26hp1iq.js
+// include: C:\Users\piostb\AppData\Local\Temp\tmps3si5juh.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\piostb\AppData\Local\Temp\tmpzotb0y61.js
+  // end include: C:\Users\piostb\AppData\Local\Temp\tmps3si5juh.js
 
 
 // Sometimes an existing Module object exists with properties
