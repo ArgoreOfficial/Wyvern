7c7b2c8d3144db9820126f67d2f88d0a4a1a6445
Argore <argoreyt@gmail.com>
2024-07-01 18:44:53 +0200

    i686-w64-mingw32 libs

--- platform/platform_windows.lua
+++ platform/platform_windows.lua
@@ -1,19 +1,32 @@
 function load_platform_windows()
     add_defines("WV_PLATFORM_WINDOWS")
-    add_requires( "glm", "glfw", "libsdl", "assimp" )
+    add_requires( "glm", "libsdl", "assimp" )
+    
+    if( is_arch( "x64" ) ) then
+        add_requires( "glfw" )
+    end
 end
 
 function target_platform_windows( target )
     local root = "../"
     
     -- icon resource
-    target:add( "files", "$(projectdir)\\resources/resource.rc" )
-    target:add( "filegroups", "Resources", { rootdir = "$(projectdir)" } )
+    -- target:add( "files", "$(projectdir)\\resources/resource.rc" )
+    -- target:add( "filegroups", "Resources", { rootdir = "$(projectdir)" } )
     
     -- add supports
     target:add( "deps", "GLAD" )
     import(root.."platform.support.glm"   )(target)
-    import(root.."platform.support.glfw"  )(target)
+    
+    if( target:is_arch( "x64" ) ) then
+        import(root.."platform.support.glfw"  )(target)
+    elseif target:is_arch( "x86" ) then
+        target:add( "ldflags", "-static-libgcc -static-libstdc++" )
+        
+        target:add( "linkdirs", "C:/msys64/mingw32/bin/" )
+        target:add( "linkdirs", "C:/msys64/mingw32/lib/" )
+    end
+
     import(root.."platform.support.assimp")(target)
     import(root.."platform.support.libsdl")(target) 
 end
--- platform/support/assimp.lua
+++ platform/support/assimp.lua
@@ -2,5 +2,13 @@
 
 function main(target)
     target:add( "defines", "WV_ASSIMP_SUPPORTED" )
-    target:add( "packages", "assimp" )
+
+    -- TODO: change x86 to winxp-x86
+    if target:is_arch( "x86" ) then
+        target:add( "includedirs", "./libs/assimp/i686-w64-mingw32/include" )
+        target:add( "linkdirs", "./libs/assimp/i686-w64-mingw32/lib/" )
+        target:add( "links", "assimp.dll" )
+    else
+        target:add( "packages", "assimp" )
+    end
 end
