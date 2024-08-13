3860af78a2cec00274d273157cd8d91b1a530f0f
Pi Östberg <piostb@psq.local>
2024-08-13 14:16:12 +0200

    build script qol

--- platform/support/assimp.lua
+++ platform/support/assimp.lua
@@ -4,11 +4,11 @@ function main(target)
     target:add( "defines", "WV_ASSIMP_SUPPORTED" )
 
     -- TODO: change x86 to winxp-x86
-    if target:is_arch( "x86" ) and os.arch() == "x64" then
-        target:add( "includedirs", "./libs/assimp/i686-w64-mingw32/include" )
-        target:add( "linkdirs", "./libs/assimp/i686-w64-mingw32/lib/" )
-        target:add( "links", "assimp.dll" )
-    else
-        target:add( "packages", "assimp" )
-    end
+    --if target:is_arch( "x86" ) and os.arch() == "x64" then
+    --    target:add( "includedirs", "./libs/assimp/i686-w64-mingw32/include" )
+    --    target:add( "linkdirs", "./libs/assimp/i686-w64-mingw32/lib/" )
+    --    target:add( "links", "assimp.dll" )
+    --else
+    --end
+    target:add( "packages", "assimp" )
 end
