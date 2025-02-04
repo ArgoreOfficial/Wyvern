893ac52ed13bdbd9ba8480cfc7e1254ec3a6a672
Argore <argoreyt@gmail.com>
2025-02-04 18:16:47 +0100

    create shortcut to .sln file on generate

--- generate-project-files.ps1
+++ generate-project-files.ps1
@@ -4,9 +4,9 @@ if( (Test-Path platform/platform_psvita.lua ) -and (Test-Path env:SCE_PSP2_SDK_D
     $platforms += ",psvita"
 }
 
-#if( (Test-Path xmake/arm_3ds/xmake.lua ) -and (Test-Path env:DEVKITARM) ) {
-    # $platforms += ",arm_3ds"
-#}
+if( (Test-Path xmake/arm_3ds/xmake.lua ) -and (Test-Path env:DEVKITARM) ) {
+    $platforms += ",arm_3ds"
+}
 
 & "xmake" f -c -y --vs=2022
 & "xmake" project -k vsxmake -y -m "Debug,Release,Package" -a $platforms ./build
@@ -15,4 +15,14 @@ if( $LASTEXITCODE -lt 0 )
 {
     write-host "Press any key to continue..."
     [void][System.Console]::ReadKey($true)
+}
+else {
+    Get-ChildItem "$(Get-Item .)\build\vsxmake2022" -Filter *.sln |
+    ForEach-Object {
+        write-host ($_.BaseName)
+        $WshShell = New-Object -COMObject WScript.Shell
+        $Shortcut = $WshShell.CreateShortcut("$(Get-Item .)\$($_.BaseName).sln.lnk")
+        $Shortcut.TargetPath = "$(Get-Item .)\build\vsxmake2022\$($_.BaseName).sln"
+        $Shortcut.Save()
+    }
 }
--- xmake.lua
+++ xmake.lua
@@ -22,13 +22,3 @@ init_platform()
 
 includes( "source/Engine" )
 includes( "source/App" )
-
--- your .lnk file
-local your_shortcut_name = "your_shortcut.lnk"      
-
--- target (file or folder) with full path
-local your_target_filespec = [["D:\Dev\wyvern\build\vsxmake2022\Sandbox.sln"]]
-
-local ps = io.popen("powershell -command -", "w")
-ps:write("$ws = New-Object -ComObject WScript.Shell;$s = $ws.CreateShortcut('"..your_shortcut_name.."');$s.TargetPath = '"..your_target_filespec.."';$s.Save()")
-ps:close()
