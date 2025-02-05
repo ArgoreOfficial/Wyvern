893e3408c7fb4d76fdb8750afc3b1ee6bbb8d082
Argore <argoreyt@gmail.com>
2025-02-05 14:04:41 +0100

    Math.h formatting and rename T to _Ty

--- source/Engine/wv/Math/Math.h
+++ source/Engine/wv/Math/Math.h
@@ -6,98 +6,109 @@
 
 namespace wv
 {
-	namespace Const /// TODO: move
-	{
-		namespace Float
-		{
-			constexpr float PI = 3.141592f;
-			constexpr float EPSILON = 0.00000001f;
-		}
+namespace Const /// TODO: move
+{
+namespace Float
+{
+constexpr float PI = 3.141592f;
+constexpr float EPSILON = 0.00000001f;
+}
 
-		namespace Double
-		{
-			constexpr float PI = 3.141592;
-			constexpr float EPSILON = 0.000001;
-		}
-	}
-
-///////////////////////////////////////////////////////////////////////////////////////
-
-	namespace Math
-	{
-		
-///////////////////////////////////////////////////////////////////////////////////////
-
-		template <typename T> T clamp( const T& _value, const T& _min, const T& _max );
-		template <typename T> T pow2 ( const T& _value );
-		template <typename T> T pow3 ( const T& _value );
-		template <typename T> T pow4 ( const T& _value );
-		template <typename T> T pow5 ( const T& _value );
-		template <typename T> T abs  ( const T& _value );
-		template <typename T> T min  ( const T& _a, const T& _b );
-		template <typename T> T max  ( const T& _a, const T& _b );
-		template <typename T> T lerp ( const T& _a, const T& _b, const T& _t );
-
-		inline float  radians( const float&  _degrees ) { return _degrees * ( Const::Float::PI  / 180.0f ); }
-		inline double radians( const double& _degrees ) { return _degrees * ( Const::Double::PI / 180.0 ); }
-
-		inline float  degrees( const float&  _radians ) { return _radians * ( 180.0f / Const::Float::PI ); }
-		inline double degrees( const double& _radians ) { return _radians * ( 180.0  / Const::Double::PI ); }
-
+namespace Double
+{
+constexpr float PI = 3.141592;
+constexpr float EPSILON = 0.000001;
+}
+}
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
-		template < typename T > T clamp( const T& _value, const T& _min, const T& _max )
-		{ 
-			const T t = _value < _min ? _min : _value;
-			return t > _max ? _max : t;
-		}
-
-		template< typename T > T pow2( const T& _value )
-		{
-			return _value * _value;
-		}
-
-		template< typename T > T pow3( const T& _value )
-		{
-			return _value * _value * _value;
-		}
-
-		template< typename T > T pow4( const T& _value )
-		{
-			return _value * _value * _value * _value;
-		}
-
-		template< typename T > T pow5( const T& _value )
-		{
-			return _value * _value * _value * _value * _value;
-		}
-
-		template< typename T > T abs( const T& _value )
-		{
-			return _value < T( 0 ) ? -_value : _value;
-		}
-
-		template< typename T > T min( const T& _a, const T& _b )
-		{
-			return _a < _b ? _a : _b;
-		}
-
-		template< typename T > T max( const T& _a, const T& _b )
-		{
-			return _a > _b ? _a : _b;
-		}
-
-		template<typename T>
-		T lerp( const T& _a, const T& _b, const T& _t )
-		{
-			return _a + _t * ( _b - _a );
-		}
-
-
+namespace Math
+{
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
-	}
+template<typename _Ty> _Ty clamp( const _Ty& _value, const _Ty& _min, const _Ty& _max );
+
+template<typename _Ty> _Ty pow2( const _Ty& _value );
+template<typename _Ty> _Ty pow3( const _Ty& _value );
+template<typename _Ty> _Ty pow4( const _Ty& _value );
+template<typename _Ty> _Ty pow5( const _Ty& _value );
+
+template<typename _Ty> _Ty abs( const _Ty& _value );
+
+template<typename _Ty> _Ty min( const _Ty& _a, const _Ty& _b );
+template<typename _Ty> _Ty max( const _Ty& _a, const _Ty& _b );
+
+template<typename _Ty> _Ty lerp( const _Ty& _a, const _Ty& _b, const _Ty& _t );
+
+inline float  radians( const float& _degrees ) { 
+	return _degrees * ( Const::Float::PI / 180.0f ); 
+}
+
+inline double radians( const double& _degrees ) { 
+	return _degrees * ( Const::Double::PI / 180.0 ); 
+}
+
+inline float  degrees( const float& _radians )  { 
+	return _radians * ( 180.0f / Const::Float::PI ); 
+}
+
+inline double degrees( const double& _radians ) { 
+	return _radians * ( 180.0 / Const::Double::PI ); 
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+template<typename _Ty> _Ty clamp( const _Ty& _value, const _Ty& _min, const _Ty& _max )
+{
+	const _Ty t = _value < _min ? _min : _value;
+	return t > _max ? _max : t;
+}
+
+template<typename _Ty> _Ty pow2( const _Ty& _value )
+{
+	return _value * _value;
+}
+
+template<typename _Ty> _Ty pow3( const _Ty& _value )
+{
+	return _value * _value * _value;
+}
+
+template<typename _Ty> _Ty pow4( const _Ty& _value )
+{
+	return _value * _value * _value * _value;
+}
+
+template<typename _Ty> _Ty pow5( const _Ty& _value )
+{
+	return _value * _value * _value * _value * _value;
+}
+
+template<typename _Ty> _Ty abs( const _Ty& _value )
+{
+	return _value < _Ty( 0 ) ? -_value : _value;
+}
+
+template<typename _Ty> _Ty min( const _Ty& _a, const _Ty& _b )
+{
+	return _a < _b ? _a : _b;
+}
+
+template<typename _Ty> _Ty max( const _Ty& _a, const _Ty& _b )
+{
+	return _a > _b ? _a : _b;
+}
+
+template<typename _Ty>
+_Ty lerp( const _Ty& _a, const _Ty& _b, const _Ty& _t )
+{
+	return _a + _t * ( _b - _a );
+}
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+}
 
 }
