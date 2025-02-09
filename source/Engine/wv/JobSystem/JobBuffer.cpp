f9eaf57323c41667f43c0fcc26998cba3cef1dd8
Argore <argoreyt@gmail.com>
2025-02-09 14:39:07 +0100

    formatting

--- source/Engine/wv/JobSystem/JobBuffer.cpp
+++ source/Engine/wv/JobSystem/JobBuffer.cpp
@@ -18,15 +18,13 @@ bool wv::JobBuffer::push( Job* _pJob )
 {
 	int32_t bottom = m_bottom.load( std::memory_order_acquire );
 
-	if ( bottom < static_cast<int32_t>( m_jobs.size() ) )
-	{
-		m_jobs[ bottom ] = _pJob;
-		m_bottom.store( bottom + 1, std::memory_order_release );
+	if ( bottom >= static_cast<int32_t>( m_jobs.size() ) )
+		return false; // queue is full
+	
+	m_jobs[ bottom ] = _pJob;
+	m_bottom.store( bottom + 1, std::memory_order_release );
 
-		return true;
-	}
-
-	return false; // queue is full
+	return true;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -39,41 +37,39 @@ wv::Job* wv::JobBuffer::pop()
 
 	int32_t t = m_top.load( std::memory_order::acquire );
 
-	if ( t <= b )
+	if ( t > b )
 	{
-		Job* job = m_jobs[ b % g_NUM_JOBS ];
-		if ( t != b )
-			return job;
-		
-		if ( !m_top.compare_exchange_weak( t, t + 1, std::memory_order::acq_rel ) )
-			job = nullptr; // failed race against steal
-		
-		m_bottom.store( t + 1, std::memory_order::release );
-		return job;
+		// queue is empty
+		m_bottom.store( t, std::memory_order::release );
+		return nullptr;
 	}
 
-	// deque was already empty
-	m_bottom.store( t, std::memory_order::release );
-	return nullptr;
+	Job* job = m_jobs[ b ];
+	if ( t != b )
+		return job;
+		
+	if ( !m_top.compare_exchange_weak( t, t + 1, std::memory_order::acq_rel ) )
+		job = nullptr; // failed race against steal
+		
+	m_bottom.store( t + 1, std::memory_order::release );
+
+	return job;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////
 
 wv::Job* wv::JobBuffer::steal()
 {
-	int32_t t = m_top.load( std::memory_order::acquire ) ;
-	std::atomic_signal_fence( std::memory_order_seq_cst );
+	int32_t t = m_top.load( std::memory_order::acquire );
 	int32_t b = m_bottom.load( std::memory_order::acquire );
 
-	if ( t < b )
-	{
-		Job* job = m_jobs[ t % g_NUM_JOBS ];
+	if ( t <= b )
+		return nullptr; // queue is empty
+	
+	Job* job = m_jobs[ t % g_NUM_JOBS ];
 		
-		if ( !m_top.compare_exchange_weak( t, t + 1, std::memory_order::acq_rel ) )
-			return nullptr; // failed race against steal or pop
+	if ( !m_top.compare_exchange_weak( t, t + 1, std::memory_order::acq_rel ) )
+		return nullptr; // failed race against steal or pop
 
-		return job;
-	}
-
-	return nullptr; // queue is empty
+	return job;
 }
