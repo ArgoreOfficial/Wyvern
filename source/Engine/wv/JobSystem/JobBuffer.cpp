47a368c9e7726e2cb1e2088eff68fdb8a9773102
Argore <argoreyt@gmail.com>
2025-02-09 14:43:34 +0100

    renamed bottom to head and top to tail

--- source/Engine/wv/JobSystem/JobBuffer.cpp
+++ source/Engine/wv/JobSystem/JobBuffer.cpp
@@ -1,8 +1,6 @@
 #include "JobBuffer.h"
 
-#include "Job.h"
-#include <wv/JobSystem/JobSystem.h>
-
+#include <wv/JobSystem/Job.h>
 #include <wv/Math/Math.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -16,13 +14,13 @@ wv::JobBuffer::JobBuffer(  )
 
 bool wv::JobBuffer::push( Job* _pJob )
 {
-	int32_t bottom = m_bottom.load( std::memory_order_acquire );
+	int32_t head = m_head.load( std::memory_order::acquire );
 
-	if ( bottom >= static_cast<int32_t>( m_jobs.size() ) )
+	if ( head >= static_cast<int32_t>( m_jobs.size() ) )
 		return false; // queue is full
 	
-	m_jobs[ bottom ] = _pJob;
-	m_bottom.store( bottom + 1, std::memory_order_release );
+	m_jobs[ head ] = _pJob;
+	m_head.store( head + 1, std::memory_order::release );
 
 	return true;
 }
@@ -31,27 +29,27 @@ bool wv::JobBuffer::push( Job* _pJob )
 
 wv::Job* wv::JobBuffer::pop()
 {
-	int32_t b = m_bottom.load( std::memory_order::acquire );
-	b = wv::Math::max<int32_t>( 0, b - 1 );
-	m_bottom.store( b, std::memory_order::release );
+	int32_t head = m_head.load( std::memory_order::acquire );
+	head = wv::Math::max<int32_t>( 0, head - 1 );
+	m_head.store( head, std::memory_order::release );
 
-	int32_t t = m_top.load( std::memory_order::acquire );
+	int32_t tail = m_tail.load( std::memory_order::acquire );
 
-	if ( t > b )
+	if ( tail > head )
 	{
 		// queue is empty
-		m_bottom.store( t, std::memory_order::release );
+		m_head.store( tail, std::memory_order::release );
 		return nullptr;
 	}
 
-	Job* job = m_jobs[ b ];
-	if ( t != b )
+	Job* job = m_jobs[ head ];
+	if ( tail != head )
 		return job;
 		
-	if ( !m_top.compare_exchange_weak( t, t + 1, std::memory_order::acq_rel ) )
+	if ( !m_tail.compare_exchange_weak( tail, tail + 1, std::memory_order::acq_rel ) )
 		job = nullptr; // failed race against steal
 		
-	m_bottom.store( t + 1, std::memory_order::release );
+	m_head.store( tail + 1, std::memory_order::release );
 
 	return job;
 }
@@ -60,15 +58,15 @@ wv::Job* wv::JobBuffer::pop()
 
 wv::Job* wv::JobBuffer::steal()
 {
-	int32_t t = m_top.load( std::memory_order::acquire );
-	int32_t b = m_bottom.load( std::memory_order::acquire );
+	int32_t tail = m_tail.load( std::memory_order::acquire );
+	int32_t head = m_head.load( std::memory_order::acquire );
 
-	if ( t <= b )
+	if ( tail <= head )
 		return nullptr; // queue is empty
 	
-	Job* job = m_jobs[ t % g_NUM_JOBS ];
+	Job* job = m_jobs[ tail % g_NUM_JOBS ];
 		
-	if ( !m_top.compare_exchange_weak( t, t + 1, std::memory_order::acq_rel ) )
+	if ( !m_tail.compare_exchange_weak( tail, tail + 1, std::memory_order::acq_rel ) )
 		return nullptr; // failed race against steal or pop
 
 	return job;
