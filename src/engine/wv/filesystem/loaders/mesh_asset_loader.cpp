d667eee9a289b0538f9507714a71f71e42d84b69
Argore <argoreyt@gmail.com>
2026-01-13 23:19:47 +0100

    cleanup

--- src/engine/wv/filesystem/loaders/mesh_asset_loader.cpp
+++ src/engine/wv/filesystem/loaders/mesh_asset_loader.cpp
@@ -19,8 +19,9 @@ struct fastgltf::ElementTraits<wv::Vector2f> : fastgltf::ElementTraitsBase<wv::V
 
 wv::ResourceID wv::MeshAssetLoader::load( const std::filesystem::path& _path )
 {
-	// TODO: actual mesh loading lmfao
 	ResourceID res{};
+	MeshAsset meshAsset{};
+	meshAsset.path = _path;
 
 	if ( m_assetManager->containsAsset( _path ) )
 	{
@@ -28,127 +29,87 @@ wv::ResourceID wv::MeshAssetLoader::load( const std::filesystem::path& _path )
 	}
 	else
 	{
-		if ( _path == "wv://cube" )
+		// Load and parse file 
+
+		fastgltf::Parser parser;
+		std::filesystem::path fullpath = m_filesystem->getFullPath( _path );
+		
+		fastgltf::Expected<fastgltf::GltfDataBuffer> data = fastgltf::GltfDataBuffer::FromPath( fullpath );
+		if ( data.error() != fastgltf::Error::None )
 		{
-			MeshAsset asset{};
-
-			asset.vertexPositions = {
-				{ -1, -1, -1 },
-				{  1, -1, -1 },
-				{  1,  1, -1 },
-				{ -1,  1, -1 },
-				{ -1, -1,  1 },
-				{  1, -1,  1 },
-				{  1,  1,  1 },
-				{ -1,  1,  1 }
-			};
-
-			asset.indices = {
-				0, 1, 3, 3, 1, 2,
-				1, 5, 2, 2, 5, 6,
-				5, 4, 6, 6, 4, 7,
-				4, 0, 7, 7, 0, 3,
-				3, 2, 7, 7, 2, 6,
-				4, 5, 0, 0, 5, 1
-			};
-
-			asset.vertexColours = {
-				{ 1.0f, 0.0f, 0.0f },
-				{ 0.0f, 1.0f, 0.0f },
-				{ 0.0f, 0.0f, 1.0f },
-				{ 0.0f, 0.0f, 0.0f },
-				{ 1.0f, 0.0f, 0.0f },
-				{ 0.0f, 1.0f, 0.0f },
-				{ 0.0f, 0.0f, 1.0f },
-				{ 0.0f, 0.0f, 0.0f },
-			};
-
-			res = m_meshAssets.emplace( asset );
+			WV_LOG_ERROR( "Failed to load model %s\n", _path.string().c_str() );
+			return {};
 		}
-		else
+
+		fastgltf::Expected<fastgltf::Asset> load = parser.loadGltf( data.get(), fullpath.parent_path(), fastgltf::Options::LoadExternalBuffers );
+		if ( load.error() != fastgltf::Error::None )
 		{
-			// Load and parse file 
+			WV_LOG_ERROR( "Failed to parse model%s\n", _path.string().c_str() );
+			return {};
+		}
 
-			fastgltf::Parser parser;
-			std::filesystem::path fullpath = m_filesystem->getFullPath( _path );
+		// Create mesh asset
+
+		fastgltf::Asset& asset = load.get();
 			
-			fastgltf::Expected<fastgltf::GltfDataBuffer> data = fastgltf::GltfDataBuffer::FromPath( fullpath );
-			if ( data.error() != fastgltf::Error::None )
+		for ( fastgltf::Mesh& mesh : asset.meshes )
+		{
+			for ( auto& primitive : mesh.primitives )
 			{
-				WV_LOG_ERROR( "Failed to load model %s\n", _path.string().c_str() );
-				return {};
-			}
+				GeometrySurface surface;
+				surface.startIndex = meshAsset.indices.size();
+				surface.count = asset.accessors[ primitive.indicesAccessor.value() ].count;
 
-			fastgltf::Expected<fastgltf::Asset> load = parser.loadGltf( data.get(), fullpath.parent_path(), fastgltf::Options::LoadExternalBuffers );
-			if ( load.error() != fastgltf::Error::None )
-			{
-				WV_LOG_ERROR( "Failed to parse model%s\n", _path.string().c_str() );
-				return {};
-			}
-
-			// Create mesh asset
-
-			fastgltf::Asset& asset = load.get();
-			MeshAsset meshAsset{};
-			meshAsset.path = _path;
-			
-			for ( fastgltf::Mesh& mesh : asset.meshes )
-			{
-				for ( auto& primitive : mesh.primitives )
+				// load indices
 				{
-					GeometrySurface surface;
-					surface.startIndex = meshAsset.indices.size();
-					surface.count = asset.accessors[ primitive.indicesAccessor.value() ].count;
-
-					// load indices
-					{
-						fastgltf::Accessor& indexAccessor = asset.accessors[ primitive.indicesAccessor.value() ];
-						size_t initialVerticesSize = meshAsset.vertexPositions.size();
+					fastgltf::Accessor& indexAccessor = asset.accessors[ primitive.indicesAccessor.value() ];
+					size_t initialVerticesSize = meshAsset.vertexPositions.size();
 						
-						fastgltf::iterateAccessor<std::uint32_t>( asset, indexAccessor, 
-							[ & ]( std::uint32_t index ) {
-								meshAsset.indices.push_back( index + initialVerticesSize );
-							} );
-					}
+					fastgltf::iterateAccessor<std::uint32_t>( asset, indexAccessor, 
+						[ & ]( std::uint32_t index ) {
+							meshAsset.indices.push_back( index + initialVerticesSize );
+						} );
+				}
 
-					// load vertex data
-					{
-						// vertex position
-						fastgltf::Accessor& positionAccessor = asset.accessors[ primitive.findAttribute( "POSITION" )->accessorIndex ];
-						fastgltf::iterateAccessorWithIndex<wv::Vector3f>( asset, positionAccessor, 
-							[ & ]( wv::Vector3f pos, std::size_t idx ) {
-								meshAsset.vertexPositions.push_back( pos );
-							} );
+				// load vertex data
+				{
+					// vertex position
+					fastgltf::Accessor& positionAccessor = asset.accessors[ primitive.findAttribute( "POSITION" )->accessorIndex ];
+					fastgltf::iterateAccessorWithIndex<wv::Vector3f>( asset, positionAccessor, 
+						[ & ]( wv::Vector3f pos, std::size_t idx ) {
+							meshAsset.vertexPositions.push_back( pos );
+						} );
 						
-						// vertex normal
-						fastgltf::Accessor& normalAccessor = asset.accessors[ primitive.findAttribute( "NORMAL" )->accessorIndex ];
-						fastgltf::iterateAccessorWithIndex<wv::Vector3f>( asset, normalAccessor,
-							[ & ]( wv::Vector3f _normal, std::size_t _idx ) {
-								meshAsset.vertexNormals.push_back( _normal );
-							} );
+					// vertex normal
+					fastgltf::Accessor& normalAccessor = asset.accessors[ primitive.findAttribute( "NORMAL" )->accessorIndex ];
+					fastgltf::iterateAccessorWithIndex<wv::Vector3f>( asset, normalAccessor,
+						[ & ]( wv::Vector3f _normal, std::size_t _idx ) {
+							meshAsset.vertexNormals.push_back( _normal );
+						} );
 						
-						// texture coordinate 0
-						fastgltf::Accessor& texCoord0Accessor = asset.accessors[ primitive.findAttribute( "TEXCOORD_0" )->accessorIndex ];
-						fastgltf::iterateAccessorWithIndex<wv::Vector2f>( asset, texCoord0Accessor,
-							[ & ]( wv::Vector2f _texCoord, std::size_t _idx ) {
-								meshAsset.vertexUVs.push_back( _texCoord );
-							} );
+					// texture coordinate 0
+					fastgltf::Accessor& texCoord0Accessor = asset.accessors[ primitive.findAttribute( "TEXCOORD_0" )->accessorIndex ];
+					fastgltf::iterateAccessorWithIndex<wv::Vector2f>( asset, texCoord0Accessor,
+						[ & ]( wv::Vector2f _texCoord, std::size_t _idx ) {
+							meshAsset.vertexUVs.push_back( _texCoord );
+						} );
 
-						// vertex colour
-						fastgltf::Accessor& colourAccessor = asset.accessors[ primitive.findAttribute( "COLOR_0" )->accessorIndex ];
-						fastgltf::iterateAccessorWithIndex<wv::Vector3f>( asset, normalAccessor,
-							[ & ]( wv::Vector3f _colour, std::size_t _idx ) {
-								meshAsset.vertexColours.push_back( _colour );
-							} );
-					}
+					// vertex colour
+					fastgltf::Accessor& colourAccessor = asset.accessors[ primitive.findAttribute( "COLOR_0" )->accessorIndex ];
+					fastgltf::iterateAccessorWithIndex<wv::Vector3f>( asset, normalAccessor,
+						[ & ]( wv::Vector3f _colour, std::size_t _idx ) {
+							meshAsset.vertexColours.push_back( _colour );
+						} );
 				}
 			}
-
-			res = m_meshAssets.emplace( meshAsset );
 		}
+
+		res = m_meshAssets.emplace( meshAsset );
 	}
 
-	m_assetManager->notifyAssetLoad( res, _path );
+	if( res.isValid() )
+		m_assetManager->notifyAssetLoad( res, _path );
+
 	return res;
 }
 
@@ -164,7 +125,8 @@ void wv::MeshAssetLoader::unload( ResourceID _resource )
 
 	if ( refcount <= 0 )
 	{
-		// TODO: actual mesh unloading
+		MeshAsset& asset = m_meshAssets.at( _resource );
+		asset = {};
 		m_meshAssets.erase( _resource );
 	}
 }
