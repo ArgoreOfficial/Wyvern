1980126bb33563d8ccef9a81ce2a2cf666b8ccea
Argore <argoreyt@gmail.com>
2026-01-18 00:43:42 +0100

    ResourceID type

--- src/engine/wv/resource_id.h
+++ src/engine/wv/resource_id.h
@@ -3,36 +3,37 @@
 #include <stdint.h>
 
 namespace wv {
-	
-class ResourceID
+
+template<typename Ty, typename Tag = void>
+class TResourceID
 {
 public:
-	ResourceID() :
-		value{ InvalidID }
-	{ }
+	TResourceID() = default;
 
-	explicit ResourceID( uint16_t _v ) :
+	explicit TResourceID( Ty _v ) :
 		value{ _v }
 	{ }
 
-	ResourceID( const ResourceID& _other ) : 
+	TResourceID( const TResourceID<Ty, Tag>& _other ) :
 		value{ _other.value } 
 	{ }
 
 	inline void invalidate() { value = InvalidID; }
 	inline bool isValid() const { return value != InvalidID; }
 
-	bool operator==( const ResourceID& _other ) const { return value == _other.value; }
-	bool operator!=( const ResourceID& _other ) const { return value != _other.value; }
-	bool operator> ( const ResourceID& _other ) const { return value > _other.value; }
-	bool operator< ( const ResourceID& _other ) const { return value < _other.value; }
-	uint16_t operator- ( const ResourceID& _other ) const { return value - _other.value; }
-	uint16_t operator+ ( const ResourceID& _other ) const { return value + _other.value; }
+	bool operator==( const TResourceID<Ty, Tag>& _other ) const { return value == _other.value; }
+	bool operator!=( const TResourceID<Ty, Tag>& _other ) const { return value != _other.value; }
+	bool operator> ( const TResourceID<Ty, Tag>& _other ) const { return value > _other.value; }
+	bool operator< ( const TResourceID<Ty, Tag>& _other ) const { return value < _other.value; }
+	Ty   operator- ( const TResourceID<Ty, Tag>& _other ) const { return value - _other.value; }
+	Ty   operator+ ( const TResourceID<Ty, Tag>& _other ) const { return value + _other.value; }
 
-	operator uint16_t() const { return value; }
+	operator Ty() const { return value; }
 
-	static constexpr uint16_t InvalidID = 0;
-	uint16_t value = InvalidID;
+	static constexpr Ty InvalidID = 0;
+	Ty value = InvalidID;
 };
+
+using ResourceID = TResourceID<uint16_t, struct ResourceID_t>;
 
 }
