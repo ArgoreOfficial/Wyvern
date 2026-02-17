95741e904029bed23f8b7226ce291a6ab4c140b5
Argore <argoreyt@gmail.com>
2026-02-17 03:41:30 +0100

    EntityComponentContainer getEntities()

--- src/engine/wv/entity/entity_component_container.h
+++ src/engine/wv/entity/entity_component_container.h
@@ -19,12 +19,28 @@ public:
 	bool registerComponent( Entity* _entity, IEntityComponent* _component );
 	bool unregisterComponent( Entity* _entity, IEntityComponent* _component );
 	
-	bool hasComponent( UUID _UUID ) const { return m_entityMap.contains( _UUID ); }
-	Entity* getEntity( UUID _UUID ) const { return hasComponent( _UUID ) ? m_entityMap.at( _UUID ) : nullptr; }
+	bool hasComponent( UUID _componentID ) const { return m_entityMap.contains( _componentID ); }
+	Entity* getEntity( UUID _componentID ) const { return hasComponent( _componentID ) ? m_entityMap.at( _componentID ) : nullptr; }
 	const std::vector<CompTy*>& getComponents() const { return m_registeredComponents; }
 
+	std::vector<Entity*> getEntities() const {
+		std::set<Entity*> entitiesSet;
+		std::vector<Entity*> entities;
+		
+		for ( auto p : m_entityMap )
+		{
+			if ( !entitiesSet.contains( p.second ) )
+			{
+				entitiesSet.insert( p.second );
+				entities.push_back( p.second );
+			}
+		}
+		
+		return entities;
+	}
+
 private:
-	std::set<UUID> m_registeredUUIDs;
+	std::set<UUID> m_registeredComponentIDs;
 	std::vector<CompTy*> m_registeredComponents;
 	std::unordered_map<UUID, Entity*> m_entityMap;
 };
@@ -36,12 +52,12 @@ inline bool EntityComponentContainer<CompTy>::registerComponent( Entity* _entity
 		return false;
 
 	UUID id = _component->getID();
-	if ( m_registeredUUIDs.contains( id ) )
+	if ( m_registeredComponentIDs.contains( id ) )
 		return false; // already registered
 
 	if ( CompTy* comp = tryCast<CompTy>( _component ) )
 	{
-		m_registeredUUIDs.insert( id );
+		m_registeredComponentIDs.insert( id );
 		m_registeredComponents.push_back( comp );
 
 		if ( _entity != nullptr )
@@ -62,23 +78,17 @@ inline bool EntityComponentContainer<CompTy>::unregisterComponent( Entity* _enti
 		return false;
 
 	UUID id = _component->getID();
-	if ( !m_registeredUUIDs.contains( id ) )
+	if ( !m_registeredComponentIDs.contains( id ) )
 		return false; // not registered
 
 	if ( CompTy* comp = tryCast<CompTy>( _component ) )
 	{
-		m_registeredUUIDs.erase( id );
+		m_registeredComponentIDs.erase( id );
 
 		if ( _entity != nullptr )
 			m_entityMap.erase( id );
 
-		for ( auto it = m_registeredComponents.begin(); it != m_registeredComponents.end(); it++ )
-		{
-			if ( comp != *it )
-				continue;
-			m_registeredComponents.erase( it );
-			break;
-		}
+		std::erase( m_registeredComponents, comp );
 	}
 	else
 		return false;
