8ac6c26a1767ba3aed31c6066228a3370557432d
Argore <argoreyt@gmail.com>
2026-04-10 18:54:32 +0200

    system.h

--- src/engine/wv/entity/ecs.h
+++ src/engine/wv/entity/ecs.h
@@ -3,7 +3,7 @@
 #include <wv/entity/entity.h>
 #include <wv/memory/memory.h>
 #include <wv/slot_map.h>
-#include <wv/updatable.h>
+#include <wv/entity/system.h>
 
 #include <bitset>
 #include <vector>
@@ -13,11 +13,8 @@ namespace wv {
 
 class Entity;
 class Archetype;
-class ECSEngine;
-class ISystem;
 
 struct ArchetypeConfig;
-struct WorldUpdateContext;
 
 static std::bitset<256> bitmaskFromComponentTypeIndices( const std::vector<int>& _componentTypeIndices )
 {
@@ -203,36 +200,6 @@ private:
 	std::unordered_map<int, IComponentContainer*> m_containers;
 	wv::SlotMap<Entity*> m_entities;
 	std::bitset<256> m_bitmask{};
-};
-
-class ISystem : public IUpdatable
-{
-	friend class ECSEngine;
-	friend class World;
-
-public:
-	std::vector<Archetype*>& getArchetypes() {
-		return m_archetypes;
-	}
-
-	std::bitset<256> getArchetypeBitmask() const { return m_archetypeBitmask; }
-	bool matchesBitmask( std::bitset<256> _bitmask ) const {
-		return ( m_archetypeBitmask & _bitmask ) == m_archetypeBitmask;
-	}
-
-	virtual void configure( ArchetypeConfig& _config ) = 0;
-
-	virtual void onComponentAdded( Archetype* _archetype, size_t _index ) { }
-	virtual void onComponentRemoved( Archetype* _archetype, size_t _index ) { }
-
-protected:
-	double deltaTime        = 0.0;
-	double physicsDeltaTime = 0.0;
-	WorldUpdateContext* updateContext = nullptr;
-
-private:
-	std::bitset<256> m_archetypeBitmask{};
-	std::vector<Archetype*> m_archetypes;
 };
 
 template<typename Ty>
