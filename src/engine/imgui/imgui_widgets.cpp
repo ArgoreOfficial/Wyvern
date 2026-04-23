ebb93ede25494c1bd80ff3c50ce4917ee7e3f362
Argore <argoreyt@gmail.com>
2026-04-23 18:10:55 +0200

    fix imgui selected widget colour

--- src/engine/imgui/imgui_widgets.cpp
+++ src/engine/imgui/imgui_widgets.cpp
@@ -7064,7 +7064,7 @@ bool ImGui::TreeNodeBehavior(ImGuiID id, ImGuiTreeNodeFlags flags, const char* l
             // Unframed typed for tree nodes
             if (hovered || selected)
             {
-                const ImU32 bg_col = GetColorU32((held && hovered) ? ImGuiCol_HeaderActive : hovered ? ImGuiCol_HeaderHovered : ImGuiCol_Header);
+                const ImU32 bg_col = GetColorU32((held && hovered) || selected ? ImGuiCol_HeaderActive : hovered ? ImGuiCol_HeaderHovered : ImGuiCol_Header);
                 RenderFrame(frame_bb.Min, frame_bb.Max, bg_col, false);
             }
             RenderNavCursor(frame_bb, id, nav_render_cursor_flags);
