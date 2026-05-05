1f05bc7b9c1fd420222326a610280f17f6865dee
Argore <argoreyt@gmail.com>
2026-05-05 22:16:08 +0200

    crate controller stuff

--- src/game/crate_component.h
+++ src/game/crate_component.h
@@ -1,8 +1,15 @@
 #pragma once
 
+#include <wv/entity/entity.h>
 #include <wv/reflection/reflection.h>
 
 struct CrateComponent
 {
-	static inline wv::Reflection reflection{}; // empty reflection
+	wv::Entity* cameraEntity;
+	wv::Vector3f positionOffset;
+
+	static inline wv::Reflection reflection{
+		wv::reflect( "cameraEntity", &CrateComponent::cameraEntity ),
+		wv::reflect( "positionOffset", &CrateComponent::positionOffset )
+	};
 };
--- src/game/crate_controller.cpp
+++ src/game/crate_controller.cpp
@@ -6,4 +6,23 @@ void CrateController::onInitialize()
 
 void CrateController::onUpdate()
 {
+	for ( wv::Archetype* archetype : getArchetypes() )
+	{
+		auto& comps = archetype->getComponents<CrateComponent>();
+		auto& entities = archetype->getEntities();
+
+		for ( size_t i = 0; i < archetype->getNumEntities(); i++ )
+		{
+			CrateComponent& crate = comps[ i ];
+			wv::Entity* entity = entities[ i ];
+
+			if ( !crate.cameraEntity )
+				continue;
+
+			wv::Vector3f pos = entity->getTransform().position;
+			pos += crate.positionOffset;
+			
+			crate.cameraEntity->getTransform().position = pos;
+		}
+	}
 }
